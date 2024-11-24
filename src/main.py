c3bb9c5554694f9819f5fcbd81936c97fe48eccc
Emi :] <186405044+imEmilyy@users.noreply.github.com>
2024-11-24 14:06:34 +0100

    test

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def main():
     while len(keys[j]) % mcd != 0:
       mcd -= 1
 
-    label_width = 125 if len(keys[j]) <= 10 else 75
+    label_width = 125 if len(keys[j]) <= 10 else 75 # If there are more than 10 elements -> width is smaller
     for i in range(len(keys[j])):            
       labelCharacter1[j][i] = tk.Label(frame[j], textvariable=labelCharacter[j][i], font=("arial", 9), image=img_tk, compound="center", fg="#151621", bd=1, relief="solid", width = label_width)
       labelCharacter1[j][i].grid(row=i // mcd, column=(i % mcd) + offset)
