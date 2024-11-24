b80d0c88754cdbc64c94b1f5733d49f8744616c0
Emi :] <186405044+imEmilyy@users.noreply.github.com>
2024-11-24 03:57:22 +0100

    it's less horrible to see

--- src/main.py
+++ src/main.py
@@ -17,18 +17,18 @@ def main():
         label1 = tk.Label(root, text = "keys counter ( you can put it even in background!!)")
         label1.pack()
         
-        for _ in range(len(keys)):
-          offset = _ * 4  
-          mcd = len(keys[_]) -1
+        for j in range(len(keys)):
+          offset = j * 4  
+          mcd = len(keys[j]) -1
 
-          while len(keys[_]) % mcd != 0:
+          while len(keys[j]) % mcd != 0:
             mcd -= 1
 
-          label_width = 125 if len(keys[_]) <= 10 else 75
-          for i in range(len(keys[_])):            
-            labelCharacter1[_][i] = tk.Label(frame[_], textvariable=labelCharacter[_][i], font=("arial", 9), image=img_tk, compound="center", fg="#151621", bd=1, relief="solid", width = label_width)
-            labelCharacter1[_][i].grid(row=i // mcd, column=(i % mcd) + offset)
-            frame[_].pack(padx = 3, pady= 10)
+          label_width = 125 if len(keys[j]) <= 10 else 75
+          for i in range(len(keys[j])):            
+            labelCharacter1[j][i] = tk.Label(frame[j], textvariable=labelCharacter[j][i], font=("arial", 9), image=img_tk, compound="center", fg="#151621", bd=1, relief="solid", width = label_width)
+            labelCharacter1[j][i].grid(row=i // mcd, column=(i % mcd) + offset)
+            frame[j].pack(padx = 3, pady= 10)
  
             
                    
